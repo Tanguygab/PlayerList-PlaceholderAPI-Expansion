05262e67e4f9aab1a360bbd4bd1afc9346da622a
Tanguygab <tanguy@gabril.com>
2023-11-07 14:45:24 +0100

    made PLACEHOLDER filter support multiple checks

--- src/main/java/io/github/tanguygab/playerlistexpansion/filters/Placeholder.java
+++ src/main/java/io/github/tanguygab/playerlistexpansion/filters/Placeholder.java
@@ -3,19 +3,27 @@ package io.github.tanguygab.playerlistexpansion.filters;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.OfflinePlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Placeholder extends Filter {
 
-    private final String leftSide;
-    private final String rightSide;
+    private final List<String[]> placeholders = new ArrayList<>();
 
     public Placeholder(String arg) {
-        String[] sides = split(arg);
-        leftSide = sides[0];
-        rightSide = sides[1];
+        for (String str : split(arg))
+            placeholders.add(str.split("="));
     }
 
     @Override
     public boolean filter(String name, OfflinePlayer viewer) {
-        return PlaceholderAPI.setPlaceholders(getOffline(name), leftSide).equals(PlaceholderAPI.setPlaceholders(viewer, rightSide));
+        for (String[] arr : placeholders)
+            if (parse(name,viewer,arr))
+                return true;
+        return false;
+    }
+
+    private boolean parse(String name, OfflinePlayer viewer, String[] arr) {
+        return PlaceholderAPI.setPlaceholders(getOffline(name), arr[0]).equals(PlaceholderAPI.setPlaceholders(viewer, arr[1]));
     }
 }
